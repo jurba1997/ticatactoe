9244681f986de2f540bf4f013fc2f12fb6437b76
jurba1997 <jurba2011@gmail.com>
2023-04-26 17:39:22 +0000

    add some more js

--- script.js
+++ script.js
@@ -8,16 +8,37 @@ window.onload = function(){
     setGame();
 }
 
-function setGame(){
-    board=[
-        [' ', ' ', ' '],
-        [' ', ' ', ' '],
-        [' ', ' ', ' ']
-    ]
+function setGame() {
+    board = [
+                [' ', ' ', ' '],
+                [' ', ' ', ' '],
+                [' ', ' ', ' ']
+            ]
 
-    for (let r=0; r < 3; r++){
-        for (let c = 0; c < 3; c++){
-            let tile = document.createElement
+    for (let r = 0; r < 3; r++) {
+        for (let c = 0; c < 3; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            if (r == 0 || r == 1) {
+                tile.classList.add("horizontal-line");
+            }
+            if (c == 0 || c == 1) {
+                tile.classList.add("vertical-line");
+            }
+            tile.innerText = "";
+            tile.addEventListener("click", setTile);
+            document.getElementById("board").appendChild(tile);
         }
     }
+}
+
+function setTile(){
+    if (gameOver){
+        return;
+    }
+
+    let coords = this.id.split("-")
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
 }
