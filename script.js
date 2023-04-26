ac5bb35896f361041ff7ea0dc860ed1aaed9d46e
jurba1997 <jurba2011@gmail.com>
2023-04-26 17:25:51 +0000

    write some js

---
+++ script.js
@@ -0,0 +1,23 @@
+var board;
+var playerO = "O";
+var playerX = "X";
+var currentPlayer = playerO
+var gameOver = false;
+
+window.onload = function(){
+    setGame();
+}
+
+function setGame(){
+    board=[
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+    ]
+
+    for (let r=0; r < 3; r++){
+        for (let c = 0; c < 3; c++){
+            let tile = document.createElement
+        }
+    }
+}
