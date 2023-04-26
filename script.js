c7fceb379a039c221d3ab7bc849c02b6b3747ee9
jurba1997 <jurba2011@gmail.com>
2023-04-26 18:01:26 +0000

    add to code to check for a winner vertically

--- script.js
+++ script.js
@@ -42,6 +42,10 @@ function setTile(){
     let r = parseInt(coords[0]);
     let c = parseInt(coords[1]);
 
+    if (board[r][c] !=' '){
+        return;
+    }
+
     board[r][c] = currentPlayer;
     this.innerText = currentPlayer;
 
@@ -51,4 +55,32 @@ function setTile(){
     else{
         currentPlayer = playerO
     }
+
+    checkWinner();
+}
+
+function checkWinner(){
+    // horizontally
+    for (let r = 0; r < 3; r++){
+        if(board[r][0] == board[r][1] && board[r][1] == board[r][2] && board[r][0] !=' '){
+            for (let i = 0; i < 3; i++){
+                let tile = document.getElementById(r.toString() + '-' + i.toString());
+                tile.classList.add("winner");
+            }
+            gameOver = true;
+            return;
+        }
+    }
+    // verically
+    for (let c = 0; c < 3; c++) {
+        if (board[0][c] == board[1][c] && board[1][c] ==  board[2][c] && board[0][c] != ' ') {
+            for (let i = 0; i < 3; i++) {
+                let tile = document.getElementById(i.toString() + "-" + c.toString());                
+                tile.classList.add("winner");
+            }
+            gameOver = true;
+            return;
+        }
+    }
+
 }
